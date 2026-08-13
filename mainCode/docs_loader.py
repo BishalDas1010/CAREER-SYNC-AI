4742a6c72c9666aa08fdaa526c558c02aee01474
BishalDas1010 <bishalbishal6659@gmail.com>
2026-08-13 22:15:26 +0530

    Rag

---
+++ mainCode/docs_loader.py
@@ -0,0 +1,75 @@
+from pathlib import Path
+
+from langchain_community.document_loaders import (
+    PyPDFLoader,
+    TextLoader,
+    CSVLoader
+    )
+
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+
+
+class pdf_upload:
+    def __init__(self, pdf_path):
+        self.pdf_path = pdf_path
+
+    # Step 1: Document type detection
+    def get_loader_type(self):
+
+        extension = Path(self.pdf_path).suffix.lower()
+
+        loaders = {
+            ".pdf": "pdf",
+            ".txt": "text",
+            ".csv": "csv",
+            ".docx": "docx",
+            ".pptx": "pptx",
+            ".html": "html",
+            ".htm": "html"
+        }
+
+        return loaders.get(extension, None)
+
+    # PDF loader
+    def pdf_loader(self):
+        loader = PyPDFLoader(self.pdf_path)
+
+        return loader.load()
+
+    def text_loader(self):
+        loader = TextLoader(self.pdf_path)
+
+        return loader.load()
+
+    def csv_loader(self):
+        loader = CSVLoader(self.pdf_path)
+        return loader.load()
+    
+
+    def document_load(self, typee):
+
+        if typee == "pdf":
+            return self.pdf_loader()
+
+        elif typee == "text":
+            return self.text_loader()
+
+        elif typee == "csv":
+            return self.csv_loader()
+
+        else:
+            raise ValueError(f"Unsupported document type: {typee}")
+
+    # Step 2: Chunking / Splitting
+    def chunking(self, docs, CHUNK_SIZE, CHUNK_OVERLAP):
+
+        text_splitter = RecursiveCharacterTextSplitter(
+            chunk_size= CHUNK_SIZE,
+            chunk_overlap= CHUNK_OVERLAP
+        )
+
+        chunks = text_splitter.split_documents(docs)
+
+        return chunks
+
+    
